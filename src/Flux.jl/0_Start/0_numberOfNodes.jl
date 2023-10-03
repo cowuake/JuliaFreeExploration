95e014df0789e87e6f1ab24c443e876d2ca2a076
r-mura <riccardo_mura@yahoo.com>
2023-10-03 11:19:55 +0200

    Expanded first example

--- src/Flux.jl/0_Start/0_numberOfNodes.jl
+++ src/Flux.jl/0_Start/0_numberOfNodes.jl
@@ -1,36 +1,40 @@
-using Flux, Plots, ProgressMeter;
+using Flux, Measurements, Plots, ProgressMeter, Statistics;
 
 f(x) = 2x - x^3;
 #f(x) = x^4 - 3*x^3 -x;
 
-n_trainings = 10;
-n_nodes = [1, 2, 5, 10, 23, 100];
+n_trainings = 100;
+n_nodes = [1 2 5 10 23 100];
+x_lower, x_upper, x_step = -2, 2, .1f0;
 
-lower = -2;
-step = .1f0;
-upper = 2;
-range = lower:step:upper;
-data = [([x], f(x)) for x in range];
+x_range = x_lower:x_step:x_upper;
+data = [([x], f(x)) for x in x_range];
 
-models = [Chain(Dense(1 => n, tanh), Dense(n => 1, bias=false), only) for n in n_nodes];
-optims = [Flux.setup(Adam(), model) for model in models];
-
-@showprogress for i in 1:length(n_nodes)
-    for epoch in 1:1000
-        Flux.train!((m, x, y) -> (m(x) - y)^2, models[i], data, optims[i]);
-    end
-end
-
+output = zeros(length(x_range), n_trainings);
 plots = [];
 
-for i in eachindex(n_nodes)
-    n = n_nodes[i];
-    p = plot(x -> f(x), lower, upper, label = "Exact function: f(x) = 2x - x³");
+@showprogress for n in n_nodes
+    for i in 1:n_trainings
+        model = Chain(Dense(1 => n, tanh), Dense(n => 1, bias=false), only);
+        optim = Flux.setup(Adam(), model);
+
+        for epoch in 1:1000
+            Flux.train!((m, x, y) -> (m(x) - y)^2, model, data, optim);
+        end
+
+        output[:, i] = [model([x]) for x in x_range];
+    end
+
+    p = plot(x -> f(x), x_lower, x_upper, label = "Exact function: f(x) = 2x - x³");
     push!(plots, p);
-    scatter!(x -> models[i]([x]), range, label = "Model output, n = $n");
+
+    average = mean(output, dims = 2);
+    stdev = stdm(output, average, dims = 2);
+    scatter!(x_range, [average[i] ± stdev[i] for i in 1:length(x_range)], label = "Model output, n = $n");
 end
 
-plot(plots[1], plots[2], plots[3], plots[4], plots[5], plots[6], layout = (2, 3), plot_title = "Sensitivity to the number of nodes", size = (1920, 1080));
+p = plot(plots[1], plots[2], plots[3], plots[4], plots[5], plots[6], layout = (2, 3), plot_title = "Neural network with a single dense layer", size = (1920, 1080));
+savefig(p, "plot.png");
 gui();
-println("Done. Press [ENTER] to exit...");
+println("Done! Press [ENTER] to exit...");
 readline();
--- src/install_deps.jl
+++ src/install_deps.jl
@@ -1,5 +1,7 @@
 using Pkg;
 
 Pkg.add("Flux");
+Pkg.add("Measurements")
 Pkg.add("Plots");
 Pkg.add("ProgressMeter");
+Pkg.add("Statistics")
